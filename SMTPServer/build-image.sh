94c4db6b2ba9ea13e0104066c095ef9c6ff8c0fe
Nono98 <noemie.plancherel@heig-vd.ch>
2021-05-01 15:23:47 +0200

    Docker OK

---
+++ SMTPServer/build-image.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Build the Docker image locally with a name
+docker build --tag mockserver .
---
+++ SMTPServer/run-container.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run container
+docker run -p 7777:7777 -p 8282:8282 mockserver
